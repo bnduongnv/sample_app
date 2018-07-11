5d9f26f7e5876ffc2a3d0459188669f0b77bf96c
duongngv <duong147nguyen@gmail.com>
2018-07-12 00:20:15 +0700

    Chapter 4 and I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root to: 'static_pages#home'
-    get 'static_pages/home'
-    get 'static_pages/help'
-    get 'static_pages/about'
-    get 'static_pages/contact'
+    get "/home", to: "static_pages#home"
+    get "/about", to: "static_pages#about"
+    get "/help", to: "static_pages#help"
+    get "/contact", to: "static_pages#contact"
   end
 end
